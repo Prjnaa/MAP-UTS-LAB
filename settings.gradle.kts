824fe37b727ca6d152c3296f84ee5eb94795451f
itsPrjnaa <88642463+Prjnaa@users.noreply.github.com>
2024-10-14 01:03:14 +0700

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IF570_LAB_UTS_Prajna Ananda Citra_00000070651"
+include(":app")
